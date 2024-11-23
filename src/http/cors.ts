34f0d2901a73a350900ada66d3cc23b790907819
Luiz Carlos <argentinaluiz@gmail.com>
2024-11-23 22:05:00 +0000

    cache - permitindo cache-control no header da requisição

--- src/http/cors.ts
+++ src/http/cors.ts
@@ -4,7 +4,7 @@ import type { CorsOptions } from "cors";
 export const defaultCorsOptions: CorsOptions = {
     //origin: "http://localhost:3000",
     origin: "*",
-    allowedHeaders: ['Content-Type', 'Authorization'] 
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'If-None-Match'], 
 }
 
 //Access-Control-Allow-Origin: http://localhost:3000
