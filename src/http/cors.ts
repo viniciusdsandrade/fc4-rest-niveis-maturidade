c73dd9f0bcb6847a3b7370105000ae4851490dcc
Luiz Carlos <argentinaluiz@gmail.com>
2024-11-23 19:06:49 +0000

    cache - primeiro exemplo com cache control

--- src/http/cors.ts
+++ src/http/cors.ts
@@ -2,6 +2,9 @@ import type { CorsOptions } from "cors";
 
 
 export const defaultCorsOptions: CorsOptions = {
-    origin: "http://localhost:3000",
+    //origin: "http://localhost:3000",
+    origin: "*",
     allowedHeaders: ['Content-Type', 'Authorization'] 
 }
+
+//Access-Control-Allow-Origin: http://localhost:3000
--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,23 +1,52 @@
-import { Router } from 'express';
-import { createCategoryService } from '../services/category.service';
+import { Router } from "express";
+import { createCategoryService } from "../services/category.service";
+import { defaultCorsOptions } from "../http/cors";
+import cors from "cors";
+
+const corsCollection = cors({
+  ...defaultCorsOptions,
+  methods: ["GET"],
+});
+
+const corsItem = corsCollection;
 
 const router = Router();
 
-router.get('/:categorySlug', async (req, res) => {
-    const categoryService = await createCategoryService();
-    const category = await categoryService.getCategoryBySlug(req.params.categorySlug);
-    res.json(category);
+router.get("/:categorySlug", corsItem, async (req, res) => {
+  const categoryService = await createCategoryService();
+  const category = await categoryService.getCategoryBySlug(
+    req.params.categorySlug
+  );
+  res.json(category);
 });
 
-router.get('/', async (req, res) => {
-    const categoryService = await createCategoryService();
-    const { page = 1, limit = 10, name } = req.query;
-    const { categories, total } = await categoryService.listCategories({
-        page: parseInt(page as string),
-        limit: parseInt(limit as string),
-        filter: { name: name as string }
-    });
-    res.json({ categories, total });
+router.get("/", corsCollection, async (req, res) => {
+  const categoryService = await createCategoryService();
+  const { page = 1, limit = 10, name } = req.query;
+  const { categories, total } = await categoryService.listCategories({
+    page: parseInt(page as string),
+    limit: parseInt(limit as string),
+    filter: { name: name as string },
+  });
+  //Pragma http 1.0
+  res.set("Cache-Control", `max-age=${20}`);
+  res.json({ categories, total });
 });
+
+router.options(
+  "/",
+  cors({
+    ...defaultCorsOptions,
+    methods: ["GET"],
+  })
+);
+
+router.options(
+  "/:categorySlug",
+  cors({
+    ...defaultCorsOptions,
+    methods: ["GET"],
+  })
+);
 
 export default router;
